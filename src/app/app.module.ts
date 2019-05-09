b6eca956cf2477491e826f47cc44dd48573d818a
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-10 01:03:48 +0300

    create service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/profile.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  private username:string;
+  private clientid: "";
+  private clientsecret: "a08f757f39d4c32a41bf3c8228d25bc51e232845";
+
+  constructor(private http: HttpClient) {
+    console.log("service is now ready!");
+    this.username = 'nashipae';
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" +  this.clientid + "&client_secret=" + this.clientsecret).map(res =>res);
+  }
+}
